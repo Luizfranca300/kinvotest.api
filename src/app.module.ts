488ddc33ecce6cd1326333e2d3aefbe9d4948eef
Luiz Fernando <luiz.fernando@kinvo.com.br>
2022-07-17 18:11:17 -0300

    app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
+import { WalletModule } from './modules/wallet/wallet.module';
 
 @Module({
-  imports: [],
+  imports: [
+    //Own modules
+    WalletModule,
+  ],
 
 })
 export class AppModule {}
